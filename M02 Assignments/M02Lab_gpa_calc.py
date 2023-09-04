104899fda558313488a298216cee2e4bd0712411
bootree700 <dmoreland14@ivytech.edu>
2023-09-04 13:44:37 -0400

    added last name variable and better documentation to M02Lab_gpa_calc.py, updated proof image -dm desktop

--- M02 Assignments/M02Lab_gpa_calc.py
+++ M02 Assignments/M02Lab_gpa_calc.py
@@ -1,19 +1,24 @@
 # Programmer Name: Dylan Moreland
 # Program Name: M02Lab_gpa_calc.py
 # Purpose: Accept student name and GPA and determine eligibility for deans list
+# Variables: last_name - stores and displays the student's last name as input by the user, gpa - stores and displays the student's gpa as input by the user
 
-student_name = str(input("Please enter the student's first name or 'ZZZ' to end: "))
+last_name = str(input("Please enter the student's last name or 'ZZZ' to end: ")) #Asks for last name input or ending statement
 
-while student_name != "ZZZ":
-    gpa = float(input("Please enter {}'s GPA: ".format(student_name)))
+#Begins a while loop, ending when last_name is set to "ZZZ"
+while last_name != "ZZZ": 
+    first_name = str(input("Please enter the student's first name: ")) #asks for first name input
 
+    gpa = float(input("Please enter %s %s's GPA: " % (first_name, last_name))) #Asks for gpa input, %s and variables after the text allows the name to be displayed in the input request
+
+    #Begins an if statement, evaluating eligibility for the Dean's List and Honor roll based upon input gpa
     if gpa >= 3.5:
-        print(student_name, "has made the Dean's List.")
+        print(first_name, last_name, "has made the Dean's List.")
     elif gpa >= 3.25:
-        print(student_name, "has made the Honor Roll")
+        print(first_name, last_name, "has made the Honor Roll")
     else:
-        print(student_name, "has not made the Dean's List or the Honor Roll. :(")
+        print(first_name, last_name, "has not made the Dean's List or the Honor Roll. :(")
     
-    student_name = str(input("Please enter the student's first name or 'ZZZ' to end: "))
+    last_name = str(input("Please enter the student's last name or 'ZZZ' to end: ")) #Reprints initial statement, breaking loop and ending the program if "ZZZ is ented"
 
-print("Thank you for using the program. Goodbye!")
+print("Thank you for using the program. Goodbye!") #Program End statement
